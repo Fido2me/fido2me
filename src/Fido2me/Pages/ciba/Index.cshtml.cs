7d68088053cbd25361be93760dbe9956eb53059c
Dmitry <dmodeus@gmail.com>
2022-08-19 16:34:21 -0400

    add CIBA pages from Duende for PoC

---
+++ src/Fido2me/Pages/ciba/Index.cshtml.cs
@@ -0,0 +1,39 @@
+// Based on Duende Software Sample
+// https://github.com/DuendeSoftware/Samples/blob/main/IdentityServer/v6/UserInteraction/Ciba/IdentityServer/Pages/Ciba/Index.cshtml.cs
+
+using System.Threading.Tasks;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Fido2me.Pages.ciba;
+
+[AllowAnonymous]
+public class IndexModel : PageModel
+{
+    public BackchannelUserLoginRequest LoginRequest { get; set; }
+
+    private readonly IBackchannelAuthenticationInteractionService _backchannelAuthenticationInteraction;
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(IBackchannelAuthenticationInteractionService backchannelAuthenticationInteractionService, ILogger<IndexModel> logger)
+    {
+        _backchannelAuthenticationInteraction = backchannelAuthenticationInteractionService;
+        _logger = logger;
+    }
+
+    public async Task<IActionResult> OnGet(string id)
+    {
+        LoginRequest = await _backchannelAuthenticationInteraction.GetLoginRequestByInternalIdAsync(id);
+        if (LoginRequest == null)
+        {
+            _logger.LogWarning("Invalid backchannel login id {id}", id);
+            return RedirectToPage("/home/error/index");
+        }
+
+        return Page();
+    }
+}
---
+++ src/Fido2me/Pages/ciba/InputModel.cs
@@ -0,0 +1,13 @@
+﻿// Based on Duende Software Sample
+// https://github.com/DuendeSoftware/Samples/blob/main/IdentityServer/v6/UserInteraction/Ciba/IdentityServer/Pages/Ciba/InputModel.cs
+
+namespace Fido2me.Pages.ciba
+{
+    public class InputModel
+    {
+        public string Button { get; set; }
+        public IEnumerable<string> ScopesConsented { get; set; }
+        public string Id { get; set; }
+        public string Description { get; set; }
+    }
+}
